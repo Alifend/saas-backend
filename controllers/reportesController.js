f305ed3b89da3d38ee8f5973f0bfd9c5597a10a3
Morphen <rios.david@correounivalle.edu.co>
2021-12-22 12:28:41 -0500

    ALGO

--- controllers/reportesController.js
+++ controllers/reportesController.js
@@ -11,7 +11,7 @@ exports.ultimosUsers = async (req, res) => {
     res.send("tenant no encontrado");
   }
   const query =
-    "SELECT u.nombre, u.createdAt FROM Tenants t INNER JOIN User_Tenant ut on t.id = ut.TenantId INNER JOIN Users u  on u.id = ut.UserId WHERE t.id = :id AND u.rol != 'admin'";
+    "SELECT u.nombre, u.createdAt FROM Tenants t INNER JOIN User_Tenant ut on t.id = ut.TenantId INNER JOIN Users u  on u.id = ut.UserId WHERE t.id = :id AND u.rol != 'admin' LIMIT 10";
 
   const result = await model.sequelize.query(query, {
     replacements: { id: tenant.id },
@@ -29,7 +29,7 @@ exports.bestSeller = async (req, res) => {
     res.send("tenant no encontrado");
   }
   const query =
-    "SELECT p.nombre, SUM(dv.cantidad) as contador FROM DetalleVenta dv INNER JOIN Products p ON dv.ProductId = p.id GROUP BY ProductId ORDER BY contador DESC ;";
+    "SELECT p.nombre, SUM(dv.cantidad) as contador FROM DetalleVenta dv INNER JOIN Products p ON dv.ProductId = p.id GROUP BY ProductId ORDER BY contador DESC LIMIT 10;";
   const result = await model.sequelize.query(query);
   res.send(result[0]);
 };
@@ -44,7 +44,7 @@ exports.bestBuyer = async (req, res) => {
     res.send("tenant no encontrado");
   }
   const query =
-    "SELECT u.nombre, SUM(v.Total) as contador FROM Venta v  INNER JOIN Users u ON v.UserId = u.id GROUP BY UserId ORDER BY contador DESC ;";
+    "SELECT u.nombre, SUM(v.Total) as contador FROM Venta v  INNER JOIN Users u ON v.UserId = u.id GROUP BY UserId ORDER BY contador DESC LIMIT 10;";
 
   const result = await model.sequelize.query(query);
   res.send(result[0]);

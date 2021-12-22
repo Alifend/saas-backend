e008508036855e95d2b7ed7ba156e9c53d4efe32
Morphen <rios.david@correounivalle.edu.co>
2021-12-21 22:28:55 -0500

    asd

--- controllers/tenantController.js
+++ controllers/tenantController.js
@@ -1,6 +1,12 @@
 const model = require("../models");
 
 exports.getTenants = async (req, res) => {
-  const tenant = await model.Tenant.findAll();
+  const tenant = await model.Tenant.findAll({
+    where: {
+      [Op.not]: 1,
+    },
+  });
   res.send(tenant);
 };
+
+exports.editTenants = async (req, res) => {};
